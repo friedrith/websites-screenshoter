74975c25b20864f94d64a2009a63e8c473fd81e1
Thibault Friedrich <thibault.friedrich@gmail.com>
2019-09-17 13:18:35 -0400

    chore: add yarn setup

---
+++ .scripts/setup.js
@@ -0,0 +1,21 @@
+import path from 'path'
+import { promises as fs } from 'fs'
+import { name } from '../package'
+
+const filenames = [path.resolve(__dirname, '../CONTRIBUTING.md')]
+
+const replaceByRealName = filename =>
+  fs
+    .readFile(filename, 'utf8')
+    .then(data => data.replace(/PROJECT_NAME/g, name))
+    .then(data => fs.writeFile(filename, data))
+    .then(data => filename)
+
+Promise.all(filenames.map(replaceByRealName))
+  .then(touchedFilenames => {
+    console.log('success:', touchedFilenames.join(', '))
+  })
+  .catch(errors => {
+    console.log(errors)
+    process.exit(2)
+  })
